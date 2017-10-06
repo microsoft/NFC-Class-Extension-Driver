c956b52f714ba0fec2483b49d7558246319fa271
Christophe Ricard <christophe-h.ricard@st.com>
2017-10-06 11:10:41 -0700

    Make phLibNfc_GetSysInfoCmd/Resp static in phLibNfc_Iso15693.c

--- libs/NfcCoreLib/lib/LibNfc/phLibNfc_Iso15693.h
+++ libs/NfcCoreLib/lib/LibNfc/phLibNfc_Iso15693.h
@@ -8,11 +8,5 @@
 
 #include "phLibNfc_Sequence.h"
 
-/* This function shall send Get system information command to ISO15693 tag */
-extern NFCSTATUS phLibNfc_GetSysInfoCmd(void *pContext,NFCSTATUS status,void *pInfo);
-
-/* This function shall Process system information received from ISO15693 tag */
-extern NFCSTATUS phLibNfc_GetSysInfoResp(void *pcontext,NFCSTATUS status,void *pInfo);
-
 extern
 phLibNfc_Sequence_t gphLibNfc_Iso15693GetSysInfoSeq[];
